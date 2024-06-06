2576b5f94424ff3ed860a1251325f5c2eac44016
ZoeMZou <mz16609@bristol.ac.uk>
2024-06-06 15:56:13 +0100

    Update codelists.py

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -1,4 +1,117 @@
+#import the function codelist_from_csv from ehrql library
 from ehrql import codelist_from_csv
+#import codelists
+# Asthma (medication using bnf, so not imported)
+ast_codelist = codelist_from_csv(
+    "codelists/opensafely-asthma-diagnosis-snomed.csv",
+    system="snomed"
+    column="id"
+)
+## !!this is bnf codes
+astmed_codelist = codelist_from_csv(
+    "codelists/bristol-asthma-medications-bnf.csv",
+    system="bnf"
+    column="code"
+)
+
+salbutamol_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-inhaler-salbutamol-medication.csv",
+    system="snomed",
+    column="id"
+)
+
+ics_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-inhaler-steroid-medication.csv",
+    system="snomed",
+    column="id"
+)
+
+pred_codes = codelist_from_csv(
+    "codelists/opensafely-asthma-oral-prednisolone-medication.csv",
+    system="snomed",
+    column="snomed_id"
+)
+# Diabetes
+# Type 1 diabetes
+diabetes_type1_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-type-1-diabetes.csv",
+    system="ctv3",
+    column="code"
+)
+
+# Type 2 diabetes
+diabetes_type2_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-type-2-diabetes.csv",
+    system="ctv3",
+    column="code"
+)
+
+# Non-diagnostic diabetes codes
+diabetes_diagnostic_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-nondiagnostic-diabetes-codes.csv",
+    system="ctv3",
+    column="code"
+)
+
+# Other or non-specific diabetes
+diabetes_other_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-other-or-nonspecific-diabetes.csv",
+    system="ctv3",
+    column="code"
+)
+
+# Gestational diabetes
+diabetes_gestational_snomed_clinical = codelist_from_csv(
+    "codelists/user-hjforbes-gestational-diabetes.csv",
+    system="ctv3",
+    column="code"
+)
+
+# Permanent diabetes
+diabetes_permanent_snomed_clinical = codelist_from_csv(
+    "codelists/opensafely-diabetes.csv",
+    system="ctv3"
+    column="CTV3ID"
+)
+
+# Insulin medication 
+insulin_snomed_clinical = codelist_from_csv(
+     "codelists/opensafely-insulin-medication.csv",
+     system="snomed",
+     column="id"
+)
+
+# Antidiabetic drugs
+antidiabetic_drugs_snomed_clinical = codelist_from_csv(
+     "codelists/opensafely-antidiabetic-drugs.csv",
+     system="snomed",
+     column="id"
+)
+
+# Antidiabetic drugs - non metformin !system snomed not dmd?
+non_metformin_dmd = codelist_from_csv(
+    "codelists/user-r_denholm-non-metformin-antidiabetic-drugs_bristol.csv", 
+    system="snomed", 
+    column="id"
+)
+
+# Type 1 diabetes secondary care
+diabetes_type1_icd10 = codelist_from_csv(
+    "codelists/opensafely-type-1-diabetes-secondary-care.csv",
+    system="icd10",
+    column="icd10_code"
+)
+
+# Type 2 diabetes secondary care
+diabetes_type2_icd10 = codelist_from_csv(
+    "codelists/user-r_denholm-type-2-diabetes-secondary-care-bristol.csv",
+    system="icd10",
+    column="code"
+)
+
+#Ethnicity 
 ethinicity_codelist = codelist_from_csv(
-    
+    "codelists/opensafely-ethnicity.csv",
+    column="Code",
+    category_column="Grouping_6"
 )
