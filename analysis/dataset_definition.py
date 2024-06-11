746ca5b272b4da7a1370c9c42553b6d90b4e19d6
ZoeMZou <mz16609@bristol.ac.uk>
2024-06-11 13:09:53 +0100

    Update dataset_definition.py

--- analysis/dataset_definition.py
+++ analysis/dataset_definition.py
@@ -1,15 +1,60 @@
-from ehrql import create_dataset
+from ehrql import codelist_from_csv, create_dataset, days
 #bring table definitions from the TPP backend 
-from ehrql.tables.tpp import patients, practice_registrations
-
+from ehrql.tables.tpp import patients, practice_registrations, clinical_events
+## Codelists from codelists.py (which pulls all variables from the codelist folder)
+from codelists import *
+# Define study start and end dates
+study_start_date = "2017-09-30"
+study_end_date = "2023-11-28"
+# Define a dataset 
 dataset = create_dataset()
+# Define population criteria 
+# Define population criteria
+dataset.define_population(
+    # All individuals in OpenSAFELY-TPP starting from 30th Sep 2017 to 28th November 2023
+    start_date=study_start_date,
+    end_date=study_end_date,
 
-index_date = "2020-03-31"
+    # Known age between 0 and 110 inclusive on the study start date
+    population=(
+        (patients.age_on(study_start_date).between(0, 110)) &
+        # Alive on the study start date
+        (patients.is_alive_on(study_start_date)) &
+        # Registered for a minimum of 365 days prior to the study start date for individuals aged 1 year old or older
+        (
+            (patients.age_on(study_start_date) >= 1) &
+            (practice_registrations.start_date <= (days(study_start_date) - 365))
+        ) |
+        # Registered since birth for those aged less than 1 year old
+        (
+            (patients.age_on(study_start_date) < 1) &
+            (practice_registrations.start_date == patients.date_of_birth)
+        ) &
+        # Known sex, deprivation, and region
+        (patients.sex.is_not_null()) &
+        (patients.deprivation.is_not_null()) &
+        (patients.region.is_not_null())
+    )
+)
+# Add additional columns to the dataset if needed
+dataset = dataset.add_columns(
+    patient_id=patients.patient_id,
+    date_of_birth=patients.date_of_birth,
+    sex=patients.sex,
+    deprivation=patients.deprivation,
+    region=patients.region,
+    age=patients.age_on(study_start_date)
+# Outcomes
+    out_
+# Covariates
+## Age
+    cov_num_age = 
 
-has_registration = practice_registrations.for_patient_on(
-    index_date
-).exists_for_patient()
+    cov_cat_age = 
+## Ethnicity 
+    cov_cat_ethnicity = 
+## Region
+    cov_cat_region =
 
-dataset.define_population(has_registration)
-
-dataset.sex = patients.sex
+)
+results = dataset.execute()
