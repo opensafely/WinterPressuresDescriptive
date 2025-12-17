2d9d57eb8988f18d90d8265681c524cd3f30a2a8
ZoeMZou <mz16609@bristol.ac.uk>
2025-12-17 10:28:29 +0000

    Update table1.R

--- analysis/table1/table1.R
+++ analysis/table1/table1.R
@@ -99,8 +99,8 @@ vars_interest <- c(
   "rurality_urban_comb"
 )
 
-# 90th percentile thresholds for each
-cutoffs <- input %>%
+# 75th percentile thresholds for each
+cutoffs_p75 <- input %>%
   summarise(
     across(
       all_of(vars_interest),
@@ -109,22 +109,32 @@ cutoffs <- input %>%
   ) %>%
   as.list()
 
+# 25th percentile thresholds for each
+cutoffs_p25 <- input %>%
+  summarise(
+    across(
+      all_of(vars_interest),
+      ~ quantile(.x, 0.25, na.rm = TRUE)
+    )
+  ) %>%
+  as.list()
+
 # add strata flags
 input <- input %>%
   mutate(
-    strata_female_above_p75 = as.integer(sex_female >= cutoffs$sex_female),
-    strata_imd1_above_p75 = as.integer(imd_1_most >= cutoffs$imd_1_most),
-    strata_eth_white_above_p75 = as.integer(
-      ethnicity_white >= cutoffs$ethnicity_white
+    strata_female_above_p75 = as.integer(sex_female >= cutoffs_p75$sex_female),
+    strata_imd1_above_p75 = as.integer(imd_1_most >= cutoffs_p75$imd_1_most),
+    strata_eth_non_white_above_p75 = as.integer(
+      ethnicity_white <= cutoffs_p25$ethnicity_white
     ),
-    strata_obesity_above_p75 = as.integer(obesity >= cutoffs$obesity),
+    strata_obesity_above_p75 = as.integer(obesity >= cutoffs_p75$obesity),
     strata_smoker_above_p75 = as.integer(
-      smoking_current >= cutoffs$smoking_current
+      smoking_current >= cutoffs_p75$smoking_current
     ),
-    strata_under5y_above_p75 = as.integer(age_0_4 >= cutoffs$age_0_4),
-    strata_age80plus_above_p75 = as.integer(age_80 >= cutoffs$age_80),
+    strata_under5y_above_p75 = as.integer(age_0_4 >= cutoffs_p75$age_0_4),
+    strata_age80plus_above_p75 = as.integer(age_80 >= cutoffs_p75$age_80),
     strata_urban_comb_above_p75 = as.integer(
-      rurality_urban_comb >= cutoffs$rurality_urban_comb
+      rurality_urban_comb >= cutoffs_p75$rurality_urban_comb
     )
   )
 
