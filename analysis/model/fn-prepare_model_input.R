d71853487dc228bb026167b05360cf1d59b80403
ZoeMZou <mz16609@bristol.ac.uk>
2025-12-19 18:26:58 +0000

    Update fn-prepare_model_input.R

--- analysis/model/fn-prepare_model_input.R
+++ analysis/model/fn-prepare_model_input.R
@@ -22,17 +22,44 @@ prepare_model_input <- function(name) {
         "_clean.rds"
     ))
 
-      ## ---- Parse covariates -------------------------------------------------------
-    cov_core_vars <- if (!is.na(active_analyses$covariate_core) &&
-                        active_analyses$covariate_core != "") {
-    unlist(strsplit(active_analyses$covariate_core, ";"))
-    } else character(0)
+    ## ---- Ensure practice_region is a factor ------------------------------------
+    region_levels <- c(
+        "North East",
+        "North West",
+        "Yorkshire and The Humber",
+        "East Midlands",
+        "West Midlands",
+        "East",
+        "London",
+        "South East",
+        "South West"
+    )
 
-    cov_other_vars <- if (!is.na(active_analyses$covariate_other) &&
-                        active_analyses$covariate_other != "") {
-    unlist(strsplit(active_analyses$covariate_other, ";"))
-    } else character(0)
+    if ("practice_region" %in% names(input)) {
+        input$practice_region <- factor(
+            input$practice_region,
+            levels = region_levels
+        )
+    }
 
+    ## ---- Parse covariates -------------------------------------------------------
+    cov_core_vars <- if (
+        !is.na(active_analyses$covariate_core) &&
+            active_analyses$covariate_core != ""
+    ) {
+        unlist(strsplit(active_analyses$covariate_core, ";"))
+    } else {
+        character(0)
+    }
+
+    cov_other_vars <- if (
+        !is.na(active_analyses$covariate_other) &&
+            active_analyses$covariate_other != ""
+    ) {
+        unlist(strsplit(active_analyses$covariate_other, ";"))
+    } else {
+        character(0)
+    }
 
     # Restrict to required variables for dataset preparation ---------------------
     print("Restrict to required variables for dataset preparation")
@@ -59,7 +86,7 @@ prepare_model_input <- function(name) {
         )
     }
 
-      ## ---- Rename to standardised names -------------------------------------------
+    ## ---- Rename to standardised names -------------------------------------------
     rename_map <- c(
         setNames(active_analyses$exposure, "exp_prop"),
         setNames(paste0("num_", active_analyses$outcome), "out_num"),
@@ -68,20 +95,19 @@ prepare_model_input <- function(name) {
 
     if (length(cov_core_vars) > 0) {
         rename_map <- c(
-        rename_map,
-        setNames(cov_core_vars, paste0("cov_core_", cov_core_vars))
+            rename_map,
+            setNames(cov_core_vars, paste0("cov_core_", cov_core_vars))
         )
     }
 
     if (length(cov_other_vars) > 0) {
         rename_map <- c(
-        rename_map,
-        setNames(cov_other_vars, paste0("cov_other_", cov_other_vars))
+            rename_map,
+            setNames(cov_other_vars, paste0("cov_other_", cov_other_vars))
         )
     }
 
     input <- dplyr::rename(input, !!!rename_map)
-
 
     # Identify final list of variables to keep -----------------------------------
     print("Identify final list of variables to keep")
@@ -91,7 +117,7 @@ prepare_model_input <- function(name) {
         "exp_prop",
         "out_num",
         "out_denom",
-        "week_number"
+        "week_number",
         paste0("cov_core_", cov_core_vars),
         paste0("cov_other_", cov_other_vars)
     )
