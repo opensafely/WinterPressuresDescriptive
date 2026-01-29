c2b1c9b3dce09de46dce14c45ed7f31491d957f0
ZoeMZou <mz16609@bristol.ac.uk>
2026-01-29 03:22:19 +0000

    Table 1 and relevant figures

--- analysis/specify_paths.R
+++ analysis/specify_paths.R
@@ -1,4 +1,6 @@
 # This file is used to specify paths. It is in the .gitignore to keep your information secret.
 
-release <- "C:/Users/mz16609/OneDrive - University of Bristol/grp-EHR - Documents/Projects/ImpactWinterPressures/release/" # Specify path to release directory
+release <- "C:/Users/mz16609/OneDrive - University of Bristol/grp-EHR - Documents/Projects/winter-pressures/gp_characteristics_and_hospital_use/release/" # Specify path to release directory
 table1 <- paste0(release, "table1/")
+table2 <- paste0(release, "table2/")
+table3 <- paste0(release, "make_output/")
