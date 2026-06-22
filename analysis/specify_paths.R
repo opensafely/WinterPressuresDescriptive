e40e7cb7930754ccfa086b897da420f17188c2f3
Zoe Zou <mz16609@bristol.ac.uk>
2026-06-22 14:54:59 +0100

    update post-release tables1&2

--- analysis/specify_paths.R
+++ analysis/specify_paths.R
@@ -1,6 +1,7 @@
 # This file is used to specify paths. It is in the .gitignore to keep your information secret.
 
-release <- "C:/Users/mz16609/OneDrive - University of Bristol/grp-EHR - Documents/Projects/winter-pressures/gp_characteristics_and_hospital_use/release/" # Specify path to release directory
+release <- "/Users/mz16609/Library/CloudStorage/OneDrive-UniversityofBristol/grp-EHR - Documents/Projects/winter-pressures/gp_characteristics_and_hospital_use/release/" # Specify path to release directory
 table1 <- paste0(release, "table1/")
 table2 <- paste0(release, "table2/")
 table3 <- paste0(release, "make_output/")
+icc_outcome <- paste0(release, "icc_outcome/")
