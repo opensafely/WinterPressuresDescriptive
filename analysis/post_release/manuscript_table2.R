a5b7a00b04943e973844fab8d679488d834bd0b8
Zoe Zou <mz16609@bristol.ac.uk>
2026-06-22 16:47:57 +0100

    Update post-release table 2

--- analysis/post_release/manuscript_table2.R
+++ analysis/post_release/manuscript_table2.R
@@ -1,5 +1,5 @@
 # Load libraries ---------------------------------------------------------------
-print('Load libraries')
+print("Load libraries")
 
 library(magrittr)
 library(tidyverse)
@@ -12,7 +12,7 @@ library(grid)
 library(gridExtra)
 
 # Specify paths ----------------------------------------------------------------
-print('Specify paths')
+print("Specify paths")
 
 # NOTE:
 # This file is used to specify paths and is in the .gitignore to keep your information secret.
@@ -22,7 +22,7 @@ print('Specify paths')
 source("analysis/specify_paths.R")
 
 # Make post-release directory --------------------------------------------------
-print('Make post-release directory')
+print("Make post-release directory")
 
 dir.create("output/post_release/", recursive = TRUE, showWarnings = FALSE)
 output_folder <- "output/post_release"
@@ -46,6 +46,11 @@ df <- file_list %>%
   }) %>%
   bind_rows()
 
+df <- df %>%
+  mutate(
+    outcome_name = str_remove(outcome_name, paste0("_", group))
+  )
+
 readr::write_csv(df, paste0(output_folder, "/table2_raw.csv"), na = "-")
 
 # Load data --------------------------------------------------------------------
@@ -59,126 +64,139 @@ df <- readr::read_csv(
 perpeople_cumu <- 1000
 perpeople_mean <- 1000
 
+# List all groups
+groups <- unique(df$group)
+
 # Filter outcomes to whole population; all practices; unplanned apc and/or ec due to all causes or any acsc conditions
-df <- df %>%
-  filter(
-    group == "main",
-    strata == "Overall",
-    acsc_condition %in% c("-", "any")
-  ) %>%
-  mutate(
-    outcome_name = str_remove(outcome_name, "_mp6.*")
-  ) %>%
-  select(
-    -group,
-    -source,
-    -apc_plan_status,
-    -acsc,
-    -acsc_condition,
-    -midpoint6,
-    -stat,
-    -strata,
-    -prop_zero
-  )
 
-# Add readable labels --------------------------------------------------------------
-print("Add readable labels")
-
-labels <- readr::read_csv("lib/labels.csv", show_col_types = FALSE)
-
-df <- merge(
-  df,
-  labels,
-  by.x = "outcome_name",
-  by.y = "term",
-  all.x = TRUE
-)
-df <- dplyr::rename(df, "outcome_label" = "label")
-
-group_order <- c(
-  "cumulative hospital use",
-  "cumulative ACSC-related hospital use",
-  "average weekly hospital use",
-  "average weekly ACSC-related hospital use"
-)
-
-df <- df %>%
-  mutate(
-    group = factor(group, levels = group_order)
-  )
-
-df_table2 <- df %>%
-  mutate(
-    median = case_when(
-      str_detect(group, "cumulative") ~ median * perpeople_cumu,
-      str_detect(group, "average") ~ median * perpeople_mean,
-      TRUE ~ median * 1000
-    ),
-    q1 = case_when(
-      str_detect(group, "cumulative") ~ q1 * perpeople_cumu,
-      str_detect(group, "average") ~ q1 * perpeople_mean,
-      TRUE ~ q1 * 1000
-    ),
-    q3 = case_when(
-      str_detect(group, "cumulative") ~ q3 * perpeople_cumu,
-      str_detect(group, "average") ~ q3 * perpeople_mean,
-      TRUE ~ q3 * 1000
-    ),
-    mean = case_when(
-      str_detect(group, "cumulative") ~ mean * perpeople_cumu,
-      str_detect(group, "average") ~ mean * perpeople_mean,
-      TRUE ~ mean * 1000
-    ),
-    sd = case_when(
-      str_detect(group, "cumulative") ~ sd * perpeople_cumu,
-      str_detect(group, "average") ~ sd * perpeople_mean,
-      TRUE ~ sd * 1000
+results <- list()
+for (g in groups) {
+  df_g <- df %>%
+    filter(
+      group == g,
+      strata == "Overall",
+      acsc_condition %in% c("-", "any")
+    ) %>%
+    mutate(
+      outcome_name = str_remove(outcome_name, "_mp6.*")
+    ) %>%
+    select(
+      -group,
+      -source,
+      -apc_plan_status,
+      -acsc,
+      -acsc_condition,
+      -midpoint6,
+      -stat,
+      -strata,
+      -prop_zero
     )
-  ) %>%
-  mutate(
-    `Median (IQR)` = sprintf(
-      "%.1f (%.1f-%.1f)",
-      median,
-      q1,
-      q3
-    ),
-    `Mean (SD)` = sprintf(
-      "%.1f (%.1f)",
-      mean,
-      sd
-    )
-  ) %>%
-  select(
-    group,
-    ref,
-    outcome_label,
-    cohort,
-    n_practices,
-    "Median (IQR)",
-    "Mean (SD)"
+  # Add readable labels --------------------------------------------------------------
+  print("Add readable labels")
+
+  labels <- readr::read_csv("lib/labels.csv", show_col_types = FALSE)
+
+  df_g <- merge(
+    df_g,
+    labels,
+    by.x = "outcome_name",
+    by.y = "term",
+    all.x = TRUE
+  )
+  df_g <- dplyr::rename(df_g, "outcome_label" = "label")
+
+  group_order <- c(
+    "cumulative hospital use",
+    "cumulative ACSC-related hospital use",
+    "average weekly hospital use",
+    "average weekly ACSC-related hospital use"
   )
 
-df_table2 <- df_table2 %>%
-  mutate(
-    cohort = factor(
+  df_g <- df_g %>%
+    mutate(
+      group = factor(group, levels = group_order)
+    )
+
+  df_table2 <- df_g %>%
+    mutate(
+      median = case_when(
+        str_detect(group, "cumulative") ~ median * perpeople_cumu,
+        str_detect(group, "average") ~ median * perpeople_mean,
+        TRUE ~ median * 1000
+      ),
+      q1 = case_when(
+        str_detect(group, "cumulative") ~ q1 * perpeople_cumu,
+        str_detect(group, "average") ~ q1 * perpeople_mean,
+        TRUE ~ q1 * 1000
+      ),
+      q3 = case_when(
+        str_detect(group, "cumulative") ~ q3 * perpeople_cumu,
+        str_detect(group, "average") ~ q3 * perpeople_mean,
+        TRUE ~ q3 * 1000
+      ),
+      mean = case_when(
+        str_detect(group, "cumulative") ~ mean * perpeople_cumu,
+        str_detect(group, "average") ~ mean * perpeople_mean,
+        TRUE ~ mean * 1000
+      ),
+      sd = case_when(
+        str_detect(group, "cumulative") ~ sd * perpeople_cumu,
+        str_detect(group, "average") ~ sd * perpeople_mean,
+        TRUE ~ sd * 1000
+      )
+    ) %>%
+    mutate(
+      `Median (IQR)` = sprintf(
+        "%.1f (%.1f-%.1f)",
+        median,
+        q1,
+        q3
+      ),
+      `Mean (SD)` = sprintf(
+        "%.1f (%.1f)",
+        mean,
+        sd
+      )
+    ) %>%
+    select(
+      group,
+      ref,
+      outcome_label,
       cohort,
-      levels = c("precovid", "postcovid1", "postcovid2", "postcovid3"),
-      labels = c(
-        "Pre-COVID",
-        "Post-COVID 1",
-        "Post-COVID 2",
-        "Post-COVID 3"
+      n_practices_midpoint6,
+      "Median (IQR)",
+      "Mean (SD)"
+    )
+
+  df_table2 <- df_table2 %>%
+    mutate(
+      cohort = factor(
+        cohort,
+        levels = c("precovid", "postcovid1", "postcovid2", "postcovid3"),
+        labels = c(
+          "Pre-COVID",
+          "Post-COVID 1",
+          "Post-COVID 2",
+          "Post-COVID 3"
+        )
       )
     )
-  )
 
-df_table2_wide <- df_table2 %>%
-  pivot_wider(
-    names_from = cohort,
-    values_from = c("n_practices", "Median (IQR)", "Mean (SD)"),
-    names_glue = "{.value} [{cohort}]"
-  ) %>%
-  arrange(group, ref)
+  df_table2_wide <- df_table2 %>%
+    pivot_wider(
+      names_from = cohort,
+      values_from = c("n_practices_midpoint6", "Median (IQR)", "Mean (SD)"),
+      names_glue = "{.value} [{cohort}]"
+    ) %>%
+    arrange(group, ref) %>%
+    mutate(
+      subgroup = g
+    )
+
+  results[[g]] <- df_table2_wide
+}
+
+df_table2_wide_all <- bind_rows(results)
 
 # # Add distribution of region and practice counts ------------------------------------------------------------
 
@@ -275,22 +293,23 @@ df_table2_wide <- df_table2 %>%
 #     df_table2_wide
 # )
 
-df_table2_wide <- df_table2_wide %>%
+df_table2_wide_all <- df_table2_wide_all %>%
   select(
+    subgroup,
     group,
     outcome_label,
-    "n_practices [Pre-COVID]",
+    "n_practices_midpoint6 [Pre-COVID]",
     "Median (IQR) [Pre-COVID]",
     "Mean (SD) [Pre-COVID]",
-    "n_practices [Post-COVID 1]",
+    "n_practices_midpoint6 [Post-COVID 1]",
     "Median (IQR) [Post-COVID 1]",
     "Mean (SD) [Post-COVID 1]",
-    "n_practices [Post-COVID 2]",
+    "n_practices_midpoint6 [Post-COVID 2]",
     "Median (IQR) [Post-COVID 2]",
     "Mean (SD) [Post-COVID 2]",
-    "n_practices [Post-COVID 3]",
+    "n_practices_midpoint6 [Post-COVID 3]",
     "Median (IQR) [Post-COVID 3]",
     "Mean (SD) [Post-COVID 3]"
   )
 
-readr::write_csv(df_table2_wide, paste0(output_folder, "/table2.csv"), na = "-")
+readr::write_csv(df_table2_wide_all, paste0(output_folder, "/table2.csv"), na = "-")
